979e7df469cfbb5d951c86455634e124a7ffa8e0
Jianannn <xu.jianan@outlook.com>
2019-05-20 20:01:29 +0800

    update signup index (html&js finished)

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -5,28 +5,28 @@ Version:2.0
 Function:lab-5 Sign Up JS
 */
 var arrInfos=[//输入规则提示文本
-    "6~18 个字符，可使用字母、数字、下划线，需以字母开头",
-    "6~16 个字符，区分大小写",
+    "请输入您的真实姓名，不超过18个字符",
+    "请输入密码，密码长度应为 6~16 个字符字母加数字组合",
     "请再次填写密码",
-    "忘记密码时，可以通过该手机号码快速找回密码",
+    "学生学号以S开头，教师工号以T开头，9位长度",
     "请填写图片中的字符，不区分大小写"
 ];
 var arrErrorInfos=[//输入报错提示文本
-    "长度应为 6~18 个字符",
-    "邮件地址需由字母、数字或下划线组成",
+    "姓名为空",
+    "姓名长度应小于18个字符",
     "密码长度应为 6~16 个字符",
     "密码过于简单，请尝试“字母+数字”的组合",
     "两次填写的密码不一致",
-    "请填写正确的手机号",
+    "请填写正确的学工号",
     "验证码输入错误",
-    "邮件地址第一位应以字母开头",
-    "密码为空"
+    "密码为空",
+    "学工号长度应为9位"
 ];
 var arrCorrectInfos=[//输入正确提示文本
-    "用户名可用",
+    "姓名可用",
     "密码可用",
     "密码一致",
-    "手机号码可用",
+    "学工号格式正确",
     "验证通过"
 ];
 var submitInfos=[//提交提示文本
@@ -46,24 +46,24 @@ var verifyCodeInfo=[//验证码内容
 ];
 var tmpCode;//定义验证码变量
 var checkStatus;//定义检查状态变量
-emailResultObj = document.getElementById("emailResult");
+nameResultObj = document.getElementById("nameResult");
 pwdResultObj = document.getElementById("pwdResult");
 pwdCheckResultObj = document.getElementById("pwdCheckResult");
-phoneResultObj = document.getElementById("phoneResult");
+IDResultObj = document.getElementById("IDResult");
 verifyCodeResultObj = document.getElementById("verifyCodeResult");
 
-emailErrorObj = document.getElementById("emailError");
+nameErrorObj = document.getElementById("nameError");
 pwdErrorObj = document.getElementById("pwdError");
 pwdCheckErrorObj = document.getElementById("pwdCheckError");
-phoneErrorObj = document.getElementById("phoneError");
+IDErrorObj = document.getElementById("IDError");
 verifyCodeErrorObj = document.getElementById("verifyCodeError");
 
 change_verifyCodePhoto();//初始化验证码
 //初始化规则提示文本
-emailResultObj.innerHTML=arrInfos[0];
+nameResultObj.innerHTML=arrInfos[0];
 pwdResultObj.innerHTML=arrInfos[1];
 pwdCheckResultObj.innerHTML=arrInfos[2];
-phoneResultObj.innerHTML=arrInfos[3];
+IDResultObj.innerHTML=arrInfos[3];
 verifyCodeResultObj.innerHTML=arrInfos[4];
 //改变验证码
 function change_verifyCodePhoto() {
@@ -78,7 +78,6 @@ function change_verifyCodePhoto() {
 //检查验证码是否正确
 function check_verifyCode() {
     var verifyCode_input=document.getElementById("verifyCode").value;
-    verifyCode_input=verifyCode_input.toUpperCase();
     verifyCode_input=verifyCode_input.toLowerCase(); //忽略验证码大小写
     if (verifyCode_input!==verifyCodeInfo[tmpCode]){ //验证码输入错误
         verifyCodeErrorObj.style.color="red";
@@ -94,23 +93,27 @@ function check_verifyCode() {
 function clear_verifyCodeError(){
     verifyCodeErrorObj.innerHTML="";
 }
-//检查手机号是否合法
-function check_phoneNum() {
-    var phoneNum_input=document.getElementById("phone").value;
-    var phoneLen=phoneNum_input.length; //获取手机号长度
-    if (phoneNum_input%1 !== 0 || phoneLen!==11){//检查手机号码是否为整数且是否为11位数
-        phoneErrorObj.style.color="red";
-        phoneErrorObj.innerHTML=arrErrorInfos[5];
+//检查学工号是否合法
+function check_ID() {
+    var IDNum_input=document.getElementById("ID").value;
+    if (IDNum_input.length !== 9) {//检查学工号长度
+        IDErrorObj.style.color="red";
+        IDErrorObj.innerHTML = arrErrorInfos[8];
         checkStatus = true;
     }
-    else { //手机号合法
-        phoneErrorObj.style.color="green";
-        phoneErrorObj.innerHTML=arrCorrectInfos[3];
+    else if (IDNum_input[0]!=="T" && IDNum_input[0]!=="S"){//检查学工号第一位
+        IDErrorObj.style.color="red";
+        IDErrorObj.innerHTML=arrErrorInfos[5];
+        checkStatus = true;
+    }
+    else { //学工号合法
+        IDErrorObj.style.color="green";
+        IDErrorObj.innerHTML=arrCorrectInfos[3];
     }
 }
-//清除phoneNumError
-function clear_phoneNumError(){
-    phoneErrorObj.innerHTML="";
+//清除IDError
+function clear_IDError(){
+    IDErrorObj.innerHTML="";
 }
 //检查密码是否合法
 function check_pwd() {
@@ -144,7 +147,7 @@ function check_pwdAgain() {
     var pwd_input2=document.getElementById("pwdCheck").value; //第二次输入的密码
     pwdCheckErrorObj.style.color="red";
     if(pwd_input2.length===0){ //第二次输入密码为空
-        pwdCheckErrorObj.innerHTML=arrErrorInfos[8];
+        pwdCheckErrorObj.innerHTML=arrErrorInfos[7];
         checkStatus = true;
     }
     else if (pwd_input1!==pwd_input2){ //两次输入密码不同
@@ -162,33 +165,27 @@ function clear_pwdAgainError(){
     pwdCheck.value=""; //清空输入框
     pwdCheckErrorObj.innerHTML="";
 }
-//检查email地址合法性
-function check_email() {
-    var addressRule = /^[a-zA-Z0-9_]+$/g; //只包含字母数字下划线
-    var firstEn = /^[A-Za-z]/g; //首位为字母
-    var address_input=document.getElementById("email").value;
-    var addressLen = address_input.length;
-    emailErrorObj.style.color="red";
-    if(addressLen < 6 || addressLen > 18){ //检查断email输入长度
-        emailErrorObj.innerHTML=arrErrorInfos[0];
+//检查name地址合法性
+function check_name() {
+    var name_input=document.getElementById("name").value;
+    var namesLen = name_input.length;
+    nameErrorObj.style.color="red";
+    if(namesLen <= 0){ //检查断name输入长度
+        nameErrorObj.innerHTML=arrErrorInfos[0];
         checkStatus = true;
     }
-    else if (!addressRule.test(address_input)){ //检查email输入是否只包含字母数字下划线
-        emailErrorObj.innerHTML=arrErrorInfos[1];
+    else if (namesLen > 18){
+        nameErrorObj.innerHTML=arrErrorInfos[1];
         checkStatus = true;
     }
-    else if (!firstEn.test(address_input)){ //检查email输入首位是否为字母
-        emailErrorObj.innerHTML=arrErrorInfos[7];
-        checkStatus = true;
-    }
-    else{ //email输入合法
-        emailErrorObj.style.color="green";
-        emailErrorObj.innerHTML=arrCorrectInfos[0];
+    else{ //name输入合法
+        nameErrorObj.style.color="green";
+        nameErrorObj.innerHTML=arrCorrectInfos[0];
     }
 }
-//清除emailError
-function clear_emailError() {
-    emailErrorObj.innerHTML="";
+//清除nameError
+function clear_nameError() {
+    nameErrorObj.innerHTML="";
 }
 //对键盘输入作出响应
 function keyTrigger(){
@@ -198,10 +195,10 @@ function keyTrigger(){
 }
 //按顺序检查所有输入项
 function check_all_status_in_order() {
-    check_email();
+    check_name();
     check_pwd();
     check_pwdAgain();
-    check_phoneNum();
+    check_ID();
     check_verifyCode();
 }
 //提交检查
