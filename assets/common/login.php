8105a6612da5a4dd936081a6b3e5904fff4146f4
yskkkkk <46449421+yskkkkk@users.noreply.github.com>
2019-05-22 13:19:30 +0800

    updateindex

--- assets/common/login.php
+++ assets/common/login.php
@@ -8,8 +8,8 @@
 <div class="main">
     <?php
 
-        $user = $_POST["user"];
-        $pwd = $_POST["password"];
+        $user = $_GET["user"];
+        $pwd = $_GET["password"];
 
         if($user==null||$pwd==null){
             header("location:../../index.html");//直接打开该php文件，跳转到登录界面
@@ -25,24 +25,45 @@
 
 
 //    $sql="SELECT * FROM stu  WHERE stu_id= 20171 AND password= '123456'";
-    $sql='SELECT * FROM stu  WHERE stu_id='.$user." AND password='".$pwd."';";
+    $sql="SELECT * FROM stu  WHERE stu_id='".$user."' AND password='".$pwd."';";
 
     $result=$db->query($sql);
     $num_users=$result->num_rows;//在数据库中搜索到符合的用户
     if($num_users!=0){
-        //搜索到该用户
-//            echo "<h3>登录成功</h3>";
-        header("location:main.html");
-//        echo "登录成功";
-    }
-    else{
-
-        header("location:../../index.html");
-        echo "<h3>登录失败</h3>";
-        echo "alert(f)";
+        $flag=1;
     }
 
+    $sql="SELECT * FROM teacher  WHERE teacher_id='".$user."' AND password='".$pwd."';";
 
+    $result=$db->query($sql);
+    $num_users=$result->num_rows;//在数据库中搜索到符合的用户
+    if($num_users!=0){
+        $flag=2;
+    }
+
+    $sql="SELECT * FROM admin  WHERE admin_id='".$user."' AND password='".$pwd."';";
+
+    $result=$db->query($sql);
+    $num_users=$result->num_rows;//在数据库中搜索到符合的用户
+    if($num_users!=0){
+        $flag=3;
+    }
+
+//    if($flag!=0){
+//        //搜索到该用户
+////            echo "<h3>登录成功</h3>";
+//        header("location:main.html");
+////        echo "登录成功";
+//    }
+//    else{
+//
+//        header("location:../../index.html");
+//        echo "<h3>登录失败</h3>";
+//        echo "alert(f)";
+//    }
+//      echo $flag;
+    response.write("<h4>".$flag."</h4>");
+    sleep(1);
     ?>
 </div>
 </body>
