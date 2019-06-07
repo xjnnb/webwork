6e1fe95e079b3a976fa23294ad7f57fdfe9f2d1e
Jianannn <xu.jianan@outlook.com>
2019-06-07 18:56:09 +0800

    Merge branch 'master' of /Users/xujianan/www/webwork with conflicts.

--- assets/common/sidebarInfo.php
+++ assets/common/sidebarInfo.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-$brr =array("username"=>$_SESSION["username"],"userLevel"=>$_SESSION["userLevel"]);
+$brr =array("username"=>$_SESSION["username"],"userLevel"=>$_SESSION["userLevel"],"permit"=>$_SESSION["permit"]);
 
 echo json_encode($brr);//输出json数据
 
--- assets/js/team.js
+++ assets/js/team.js
@@ -11,14 +11,14 @@ $(document).ready(function () {
         console.log(json);
         var html="";
         html+="<div class=\"col-sm-3 col-md-3 col-lg-3\">\n" +
-            "                                                        <div class=\"card sm-card\" style=\"background-color: #f2f3f8;border-radius: 10px;cursor:pointer;\">\n" +
+            "                                                        <div class=\"card sm-card\" style=\"background-color: #f2f3f8;border-radius: 10px;\">\n" +
             "                                                            <div class=\"icon-preview addTeam\" style=\"font-size: inherit;font-size: 6rem;  text-align: center;\"><i class=\"la la-plus\"></i></div>\n" +
             "                                                        </div>\n" +
             "                                                    </div>";
         for(var i=0;i<json.length;i++){
             html+="<div class=\"col-sm-3 col-md-3 col-lg-3\">\n" +
                 "                                                        <div class=\"card sm-card\">\n" +
-                "                                                            <img class=\"card-img-top\" src=\"../img/team.jpeg\" alt=\"Gallery Image 3\">\n" +
+                "                                                            <img class=\"card-img-top\" src=\"http://cn.inspinia.cn/html/inspiniacn/quillpro/assets/img/gallery-image-3.jpg\" alt=\"Gallery Image 3\">\n" +
                 "                                                            <div class=\"card-body\">\n" +
                 "                                                                <h4 class=\"card-title\">"+json[i].team_name+"</h4>\n" +
                 "                                                                <p class=\"card-text\"><small>"+json[i].simple+"</small></p>\n" +
@@ -99,15 +99,15 @@ $(document).ready(function () {
             console.log(json);
             function showInfo() {
                 var html = "";
-                html += "<div class=\"col-sm-3 col-md-3 col-lg-3\">\n" +
-                    "                                                        <div class=\"card sm-card\" style=\"background-color: #f2f3f8;border-radius: 10px;cursor:pointer;\">\n" +
-                    "                                                            <div class=\"icon-preview addTeam\" style=\"font-size: inherit;font-size: 6rem; text-align: center;\"><i class=\"la la-plus\"></i></div>\n" +
+                html+="<div class=\"col-sm-3 col-md-3 col-lg-3\">\n" +
+                    "                                                        <div class=\"card sm-card\" style=\"background-color: #f2f3f8;border-radius: 10px;\">\n" +
+                    "                                                            <div class=\"icon-preview addTeam\" style=\"font-size: inherit;font-size: 6rem;  text-align: center;\"><i class=\"la la-plus\"></i></div>\n" +
                     "                                                        </div>\n" +
                     "                                                    </div>";
                 for (var i = 0; i < json.length; i++) {
                     html += "<div class=\"col-sm-3 col-md-3 col-lg-3\">\n" +
                         "                                                        <div class=\"card sm-card\">\n" +
-                        "                                                            <img class=\"card-img-top\" src=\"../img/team.jpeg\" alt=\"Gallery Image 3\">\n" +
+                        "                                                            <img class=\"card-img-top\" src=\"http://cn.inspinia.cn/html/inspiniacn/quillpro/assets/img/gallery-image-3.jpg\" alt=\"Gallery Image 3\">\n" +
                         "                                                            <div class=\"card-body\">\n" +
                         "                                                                <h4 class=\"card-title\">" + json[i].team_name + "</h4>\n" +
                         "                                                                <p class=\"card-text\"><small>" + json[i].simple + "</small></p>\n" +
@@ -126,17 +126,19 @@ $(document).ready(function () {
                         "                                                    </div>";
                 }
                 $("#selectInfoTable").html(html);
-                //metchHeight
-                var $divClass = $('.sm-card');
-                var height = 0;
-                $divClass.each(function () {
-                    if ($(this).height() > height) {
-                        height = $(this).height();
-                    }
-                });
-                $divClass.height(height);
-                $('.icon-preview').css('marginTop',height/3);
             }
+
+            //metchHeight
+            var $divClass = $('.sm-card');
+            var height = 0;
+            $divClass.each(function () {
+                if ($(this).height() > height) {
+                    height = $(this).height();
+                }
+            });
+            $divClass.height(height);
+            $('.icon-preview').css('marginTop',height/3);
+
 
             //pagination
             var json = JSON.parse(data);
@@ -180,8 +182,6 @@ $(document).ready(function () {
             })
         })
     })
-
-
     $("body").on("click", ".addTeam", function () {
 
         $("#main1").hide();
